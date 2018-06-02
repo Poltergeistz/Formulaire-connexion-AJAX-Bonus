259c4de7a66939a8d8ce8cece6c03a1f81016c33
david <david.journo.31@gmail.com>
2018-06-02 15:47:50 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+var pseudo = document.getElementById('pseudo')
+var pseudoContainer = document.create()
+var pwd = document.getElementById('')
+var userWelcome = "Bienvenue ";
+
+pseudo.innerHTML =  userWelcome; 
